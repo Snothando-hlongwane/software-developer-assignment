56af5cfde8769eb79e5fa6417f4154563cfabfff
Snothando-hlongwane <122883483+Snothando-hlongwane@users.noreply.github.com>
2023-01-20 11:57:51 +0200

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,41 @@
+/*Snothando Hlongwane*/
+import React from 'react'
+
+import React, {Component} from 'react'; 
+class Home extends Component {
+    render(){
+        return (
+            <div>
+                <h2>Hello!</h2>
+                <p> always knew from a young age that I was a creative person,
+                but didn't necessarily know how to express my creativity,  or where to direct it.  
+                However, over time, I learned that invention and innovation were my true passions, 
+                allowing me to take full advantage of my creative skill set.  
+                I have a strong desire to be part of something big enough to change the world for the better
+                through technology. 
+
+                In high school, I found a way to express my creativity when I pursued the Science stream
+                alongside the Information Technology subject, which has been significant in guiding me on my software
+                development career path.
+                I now find myself as a Computer Science graduate at the University of Western Cape, where I acquired the
+                necessary skills and knowledge towards become a software developer.
+
+                Since my first exposure to Information Technology, I got to apply my creativity in several different ways:
+                projects and Information Technology related competitions, and got to learn more and more. 
+                My confidence and vision into what space I want to be involved in as an individual has been clear ever
+                since.  
+
+                I can now identify myself as an individual whose passion is directed towards being a software developer.
+                I am also an individual who values hard-work and dedication along with honesty to achieve any goal set. 
+                Finally all this vision I had from a young age since high is my main motivation for applying at SovTech. 
+                I believe that SovTech is a perfect environment for me to take a step further towards building my career, and 
+                I also believe at SovTech I can fully develop as individual, personal and professionally.</p>
+
+            </div>
+
+        );
+    }
+}
+
+
+export default Home;
